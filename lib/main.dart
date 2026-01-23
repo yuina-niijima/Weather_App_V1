245a2ca7c0630cd084a11d795ec78d49684fdab7
yuu123456 <yuukunn3777@gmail.com>
2026-01-23 19:06:20 +0900

    add: formatter trailing_commas

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello World!',
+          ),
+        ),
+      ),
     );
   }
 }
