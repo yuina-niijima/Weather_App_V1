86832a98ceb9bcb50f6e210271a21e70e752dc37
新島由唯奈 <yuina-niijima@greensys.co.jp>
2026-01-28 15:17:30 +0900

    スプラッシュ画像表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  // 1. バインディングの初期化
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // 2. スプラッシュ画面を保持（自動で消えないようにする）
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _init(); // ここでスプラッシュを制御する関数を呼ぶ
+  }
+
+  void _init() async {
+    // ★ ここで「2秒間」待機します
+    await Future.delayed(const Duration(seconds: 2));
+
+    // 3. 2秒経ったらスプラッシュを消して、アプリ画面を表示する
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-            'Hello World!',
-          ),
+          child: Text('Hello World!'),
         ),
       ),
     );
