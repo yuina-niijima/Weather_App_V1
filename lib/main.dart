d49751a9acb5f52f7f1c01b3221b4b4f31ae3eb4
新島由唯奈 <yuina-niijima@greensys.co.jp>
2026-01-30 16:34:05 +0900

    都道府県画面実装

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class MainHomePage extends StatelessWidget {
           backgroundColor: Colors.blue,
           foregroundColor: Colors.white,
         ),
-        // 1. 画面全体の余白を Padding ウィジェットで設定
+        // 画面全体の余白を Padding ウィジェットで設定
         body: Padding(
           padding: const EdgeInsets.all(24.0), // 上下左右の余白
           child: Column(
@@ -135,12 +135,120 @@ class MainHomePage extends StatelessWidget {
 class SelectCityPage extends StatelessWidget {
   const SelectCityPage({super.key});
 
+  // 都道府県のリストデータ
+  final List<String> prefectures = const [
+    '北海道',
+    '青森県',
+    '岩手県',
+    '宮城県',
+    '秋田県',
+    '山形県',
+    '福島県',
+    '茨城県',
+    '栃木県',
+    '群馬県',
+    '埼玉県',
+    '千葉県',
+    '東京都',
+    '神奈川県',
+    '新潟県',
+    '富山県',
+    '石川県',
+    '福井県',
+    '山梨県',
+    '長野県',
+    '岐阜県',
+    '静岡県',
+    '愛知県',
+    '三重県',
+    '滋賀県',
+    '京都府',
+    '大阪府',
+    '兵庫県',
+    '奈良県',
+    '和歌山県',
+    '鳥取県',
+    '島根県',
+    '岡山県',
+    '広島県',
+    '山口県',
+    '徳島県',
+    '香川県',
+    '愛媛県',
+    '高知県',
+    '福岡県',
+    '佐賀県',
+    '長崎県',
+    '熊本県',
+    '大分県',
+    '宮崎県',
+    '鹿児島県',
+    '沖縄県',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('都道府県を選択')),
-      body: const Center(
-        child: Text('都道府県のリスト', style: TextStyle(fontSize: 20)),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0), // 上下左右に余白
+        itemCount: prefectures.length, // prefecturesの数を数える
+        // 一個ずつのボタンの設定
+        itemBuilder: (context, index) {
+          return Padding(
+            // ボタンとボタンの間の隙間padding
+            padding: const EdgeInsets.only(
+              bottom: 12.0,
+            ), // ボタンの枠と、中の文字の間に隙間を空けるためpadding
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                // 文字色
+                foregroundColor: const Color.fromARGB(255, 5, 40, 57),
+                elevation: 5, // 影の深さ
+                shadowColor: Colors.indigo, // 影の色
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => WeatherDetailPage(
+                      cityName: prefectures[index],
+                    ),
+                    fullscreenDialog: true, // true だとモーダル遷移になる
+                  ),
+                );
+              },
+              child: Text(
+                prefectures[index],
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class WeatherDetailPage extends StatelessWidget {
+  final String cityName;
+
+  const WeatherDetailPage({
+    super.key,
+    required this.cityName,
+  });
+
+  // モーダル画面
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SizedBox(
+        width: double.infinity,
+        child: Text(cityName),
       ),
     );
   }
