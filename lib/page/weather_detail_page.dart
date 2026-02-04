3f1d9dfb0cb9d25681ac2ccf1bd87597318faf0b
新島由唯奈 <yuina-niijima@greensys.co.jp>
2026-02-04 14:58:40 +0900

    Revert "エラーダイアログ追加"
    This reverts commit 28cd7efb84aba644edef1ad43673bb8b4ffe8889.

--- lib/page/weather_detail_page.dart
+++ lib/page/weather_detail_page.dart
@@ -17,8 +17,6 @@ class WeatherDetailPage extends StatelessWidget {
 
   // APIからデータを取ってくる関数
   Future<WeatherData> fetchWeather() async {
-    // インジケーターを確認するため三秒待機
-    await Future.delayed(const Duration(seconds: 3));
     final url =
         'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=${AppConfig.apiKey}&units=metric&lang=ja';
     final response = await http.get(Uri.parse(url));
@@ -30,31 +28,6 @@ class WeatherDetailPage extends StatelessWidget {
     }
   }
 
-  void showErrorDialog(BuildContext context, String message) {
-    // ビルドが終わった後に実行されるように予約する
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      showDialog(
-        context: context,
-        barrierDismissible: false, // ダイアログ外タップで閉じないようにする
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('エラー'),
-            content: Text(message),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(); // ダイアログを閉じる
-                  Navigator.of(context).pop(); // 前の画面（一覧など）に戻る
-                },
-                child: const Text('戻る'),
-              ),
-            ],
-          );
-        },
-      );
-    });
-  }
-
   // 表示
   @override
   Widget build(BuildContext context) {
@@ -63,26 +36,9 @@ class WeatherDetailPage extends StatelessWidget {
       body: FutureBuilder<WeatherData>(
         future: fetchWeather(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Stack(
-              children: [
-                const ModalBarrier(
-                  dismissible: false, // 壁をタップしても消えないようにする
-                  color: Colors.black12,
-                ),
-                const Center(
-                  child: CircularProgressIndicator(), // くるくる回るインジケーター
-                ),
-              ],
-            );
-          }
-          if (snapshot.hasError) {
-            showErrorDialog(context, snapshot.error.toString());
-            return const SizedBox.shrink();
-          }
           final weather = snapshot.data;
           if (weather == null) {
-            return const Center(child: Text('データが見つかりませんでした'));
+            return SizedBox.shrink();
           }
           return Center(
             child: Column(
